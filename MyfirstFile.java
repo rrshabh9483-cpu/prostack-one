bf06fc912c86e66b564b70e742cd77373349a1e5
rrshabh9483-cpu <rrshabh9483@gmail.com>
2025-11-19 10:04:21 +0530

    file name has been changed

---
+++ MyfirstFile.java
@@ -0,0 +1,14 @@
+
+import javax.xml.crypto.Data;
+
+public class MyfirstFile {
+    private int OfferID;
+    private String OfferElectionID;
+    private Data startDate;
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        System.out.println("my name is Rishu kumar i am just testing this file for git hub");
+    }
+}
--- test2.java
+++
@@ -1,9 +0,0 @@
-public class OfferTypes {
-    private int OfferID;
-    private String OfferElectionID;
-    private Date startDate;
-
-    public static void main(String[] args){
-        Sout.println("Hello World");
-    }
-}
