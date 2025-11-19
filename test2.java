b466748aa9aa4ce471637a6c91bae680e83f2fa2
rrshabh9483-cpu <rrshabh9483@gmail.com>
2025-11-19 08:14:52 +0530

    two java files have been added

---
+++ test2.java
@@ -0,0 +1,9 @@
+public class OfferTypes {
+    private int OfferID;
+    private String OfferElectionID;
+    private Date startDate;
+
+    public static void main(String[] args){
+        Sout.println("Hello World");
+    }
+}
